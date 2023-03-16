a077e06d55e4b9e5a29c2006302aa4c4e1a02049
wbmnfktr <wbmnfktr@webmanufaktur.net>
2023-03-16 23:42:09 +0100

    Added cache busting for CSS and JS

--- src/_data/config.js
+++ src/_data/config.js
@@ -1,8 +1,11 @@
+const timestamp = new Date();
+
 module.exports = function () {
-    return {
-        title: "ProcessWire Recipes",
-        description: "A Collection of ProcessWire Recipes (Code Snippets).",
-        domain: process.env.DOMAIN || "localhost:8080",
-        url: process.env.URL || "http://localhost:8080",
-    };
+  return {
+    title: "ProcessWire Recipes",
+    description: "A Collection of ProcessWire Recipes (Code Snippets).",
+    domain: process.env.DOMAIN || "localhost:8080",
+    url: process.env.URL || "http://localhost:8080",
+    timestamp: timestamp * 1000,
+  };
 };
