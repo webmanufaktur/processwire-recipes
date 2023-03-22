4fcc00dd55b0068262921224f3e75d78ced80034
wbmnfktr <wbmnfktr@webmanufaktur.net>
2023-03-22 21:00:20 +0100

    Added PW versions as reference

--- src/_data/config.js
+++ src/_data/config.js
@@ -7,5 +7,7 @@ module.exports = function () {
     domain: process.env.DOMAIN || "localhost:8080",
     url: process.env.URL || "http://localhost:8080",
     timestamp: timestamp * 1000,
+    pwmaster: "3.0.210",
+    pwdev: "3.0.214",
   };
 };
