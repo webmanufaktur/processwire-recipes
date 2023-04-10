b0e0262a87c95dea53cb7bf3a4d84a3ae7212bc8
webmanufaktur <info@webmanufaktur.net>
2023-04-10 19:42:43 +0200

    Clean up app.js

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,23 +1,23 @@
 document.addEventListener("alpine:init", () => {
-  console.log(123),
+  console.log("AlpineJS is ready to go"),
     Alpine.store("pwr", {
       static: Alpine.$persist({
         version: 1,
       }).as("pwrecipes"),
 
-      frank: true,
-      joshi: true,
+      // frank: true,
+      // joshi: true,
 
-      toggleFrank() {
-        this.joshi = !this.joshi;
-        console.log(this.joshi);
-      },
+      // toggleFrank() {
+      //   this.joshi = !this.joshi;
+      //   console.log(this.joshi);
+      // },
     });
 });
 
-console.log("app.js is available");
+console.log("app.js is ready to go");
 
 // we check for updates on local storage and reload all browser showing this website
-window.addEventListener("storage", () => {
-  location.reload();
-});
+// window.addEventListener("storage", () => {
+//   location.reload();
+// });
