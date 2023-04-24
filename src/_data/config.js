de58280c04fbb349e4777b9e9947b3ef0e7526c6
webmanufaktur <info@webmanufaktur.net>
2023-04-24 22:34:43 +0200

    Added base for tracking and testing

--- src/_data/config.js
+++ src/_data/config.js
@@ -1,5 +1,12 @@
 const timestamp = new Date();
 
+const environment = process.env.ELEVENTY_ENV;
+const PROD_STATE = "PROD";
+const prodUrl = "https://processwire.recipes";
+const devUrl = "http://localhost:8080";
+const isProd = environment === PROD_STATE;
+const baseUrl = environment === PROD_STATE ? prodUrl : devUrl;
+
 module.exports = function () {
   return {
     title: "ProcessWire Recipes",
@@ -7,7 +14,10 @@ module.exports = function () {
     domain: process.env.DOMAIN || "localhost:8080",
     url: process.env.URL || "http://localhost:8080",
     timestamp: timestamp * 1000,
-    pwmaster: "3.0.210",
-    pwdev: "3.0.214",
+    environment,
+    prodUrl,
+    devUrl,
+    baseUrl,
+    isProd,
   };
 };
